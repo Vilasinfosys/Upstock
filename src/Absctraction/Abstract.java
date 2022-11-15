985d43f6718e3a50d6940436897e71d3e919766b
Vilasinfosys <118238793+Vilasinfosys@users.noreply.github.com>
2022-11-15 10:05:01 +0530

    Update Abstract.java

--- src/Absctraction/Abstract.java
+++ src/Absctraction/Abstract.java
@@ -3,7 +3,7 @@ package Absctraction;
 abstract public class Abstract {
 	public void m1(){ // complate method
 		System.out.println(" Boss i am already complated");
-		System.out.println("Vilas")
+		System.out.println("Vilas");
 		}
 	abstract public void m2();
 	abstract public void m3();
